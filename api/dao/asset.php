0554548108ae71fb673a2e9266914f84137d48fe
Jeff Standen <jeff@webgroupmedia.com>
2013-05-07 15:50:57 -0700

    Implemented fieldset subtotals and filters for asset worklists.

--- api/dao/asset.php
+++ api/dao/asset.php
@@ -221,6 +221,10 @@ class DAO_Asset extends Cerb_ORMHelper {
 				$args['has_multiple_values'] = true;
 				self::_searchComponentsVirtualContextLinks($param, $from_context, $from_index, $args['join_sql'], $args['where_sql']);
 				break;
+				
+			case SearchFields_Asset::VIRTUAL_HAS_FIELDSET:
+				self::_searchComponentsVirtualHasFieldset($param, $from_context, $from_index, $args['join_sql'], $args['where_sql']);
+				break;
 		
 			case SearchFields_Asset::VIRTUAL_WATCHERS:
 				$args['has_multiple_values'] = true;
@@ -301,6 +305,7 @@ class SearchFields_Asset implements IDevblocksSearchFields {
 	const UPDATED_AT = 'a_updated_at';
 
 	const VIRTUAL_CONTEXT_LINK = '*_context_link';
+	const VIRTUAL_HAS_FIELDSET = '*_has_fieldset';
 	const VIRTUAL_WATCHERS = '*_workers';
 	
 	const CONTEXT_LINK = 'cl_context_from';
@@ -318,6 +323,7 @@ class SearchFields_Asset implements IDevblocksSearchFields {
 			self::UPDATED_AT => new DevblocksSearchField(self::UPDATED_AT, 'asset', 'updated_at', $translate->_('common.updated'), Model_CustomField::TYPE_DATE),
 
 			self::VIRTUAL_CONTEXT_LINK => new DevblocksSearchField(self::VIRTUAL_CONTEXT_LINK, '*', 'context_link', $translate->_('common.links'), null),
+			self::VIRTUAL_HAS_FIELDSET => new DevblocksSearchField(self::VIRTUAL_HAS_FIELDSET, '*', 'has_fieldset', $translate->_('common.fieldset'), null),
 			self::VIRTUAL_WATCHERS => new DevblocksSearchField(self::VIRTUAL_WATCHERS, '*', 'workers', $translate->_('common.watchers'), 'WS'),
 			
 			self::CONTEXT_LINK => new DevblocksSearchField(self::CONTEXT_LINK, 'context_link', 'from_context', null),
@@ -366,6 +372,7 @@ class View_Asset extends C4_AbstractView implements IAbstractView_Subtotals {
 		
 		$this->addColumnsHidden(array(
 			SearchFields_Asset::VIRTUAL_CONTEXT_LINK,
+			SearchFields_Asset::VIRTUAL_HAS_FIELDSET,
 			SearchFields_Asset::VIRTUAL_WATCHERS,
 		));
 		
@@ -397,7 +404,7 @@ class View_Asset extends C4_AbstractView implements IAbstractView_Subtotals {
 	}
 
 	function getSubtotalFields() {
-		$all_fields = $this->getParamsAvailable();
+		$all_fields = $this->getParamsAvailable(true);
 		
 		$fields = array();
 
@@ -413,6 +420,7 @@ class View_Asset extends C4_AbstractView implements IAbstractView_Subtotals {
 					
 				// Virtuals
 				case SearchFields_Asset::VIRTUAL_CONTEXT_LINK:
+				case SearchFields_Asset::VIRTUAL_HAS_FIELDSET:
 				case SearchFields_Asset::VIRTUAL_WATCHERS:
 					$pass = true;
 					break;
@@ -449,6 +457,10 @@ class View_Asset extends C4_AbstractView implements IAbstractView_Subtotals {
 				
 			case SearchFields_Asset::VIRTUAL_CONTEXT_LINK:
 				$counts = $this->_getSubtotalCountForContextLinkColumn('DAO_Asset', 'cerberusweb.contexts.asset', $column);
+				break;
+				
+			case SearchFields_Asset::VIRTUAL_HAS_FIELDSET:
+				$counts = $this->_getSubtotalCountForHasFieldsetColumn('DAO_Asset', 'cerberusweb.contexts.asset', $column);
 				break;
 				
 			case SearchFields_Asset::VIRTUAL_WATCHERS:
@@ -509,6 +521,10 @@ class View_Asset extends C4_AbstractView implements IAbstractView_Subtotals {
 				$tpl->display('devblocks:cerberusweb.core::internal/views/criteria/__context_link.tpl');
 				break;
 				
+			case SearchFields_Asset::VIRTUAL_HAS_FIELDSET:
+				$this->_renderCriteriaHasFieldset($tpl, 'cerberusweb.contexts.asset');
+				break;
+				
 			case SearchFields_Asset::VIRTUAL_WATCHERS:
 				$tpl->display('devblocks:cerberusweb.core::internal/views/criteria/__context_worker.tpl');
 				break;
@@ -545,6 +561,10 @@ class View_Asset extends C4_AbstractView implements IAbstractView_Subtotals {
 				$this->_renderVirtualContextLinks($param);
 				break;
 			
+			case SearchFields_Asset::VIRTUAL_HAS_FIELDSET:
+				$this->_renderVirtualHasFieldset($param);
+				break;
+				
 			case SearchFields_Asset::VIRTUAL_WATCHERS:
 				$this->_renderVirtualWatchers($param);
 				break;
@@ -579,6 +599,11 @@ class View_Asset extends C4_AbstractView implements IAbstractView_Subtotals {
 			case SearchFields_Asset::VIRTUAL_CONTEXT_LINK:
 				@$context_links = DevblocksPlatform::importGPC($_REQUEST['context_link'],'array',array());
 				$criteria = new DevblocksSearchCriteria($field,DevblocksSearchCriteria::OPER_IN,$context_links);
+				break;
+				
+			case SearchFields_Asset::VIRTUAL_HAS_FIELDSET:
+				@$options = DevblocksPlatform::importGPC($_REQUEST['options'],'array',array());
+				$criteria = new DevblocksSearchCriteria($field,DevblocksSearchCriteria::OPER_IN,$options);
 				break;
 				
 			case SearchFields_Asset::VIRTUAL_WATCHERS:
