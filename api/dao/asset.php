23d3c476ef16b436b4935aa901a3a552318fc83c
Jeff Standen <jeff@webgroupmedia.com>
2014-07-23 00:39:06 -0700

    [Code Cleanup]

--- api/dao/asset.php
+++ api/dao/asset.php
@@ -282,12 +282,8 @@ class DAO_Asset extends Cerb_ORMHelper {
 		$results = array();
 		
 		while($row = mysqli_fetch_assoc($rs)) {
-			$result = array();
-			foreach($row as $f => $v) {
-				$result[$f] = $v;
-			}
 			$object_id = intval($row[SearchFields_Asset::ID]);
-			$results[$object_id] = $result;
+			$results[$object_id] = $row;
 		}
 
 		$total = count($results);
