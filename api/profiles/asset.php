f47ae4c65e66d7afc6a505fd8851a3be46d1dff7
Jeff Standen <jeff@webgroupmedia.com>
2016-03-02 16:14:26 -0800

    [Code Cleanup]

--- api/profiles/asset.php
+++ api/profiles/asset.php
@@ -50,13 +50,13 @@ class PageSection_ProfilesAsset extends Extension_PageSection {
 		$properties = array();
 			
 		$properties['name'] = array(
-			'label' => ucfirst($translate->_('common.name')),
+			'label' => mb_ucfirst($translate->_('common.name')),
 			'type' => Model_CustomField::TYPE_SINGLE_LINE,
 			'value' => $asset->name,
 		);
 			
 		$properties['updated'] = array(
-			'label' => ucfirst($translate->_('common.updated')),
+			'label' => mb_ucfirst($translate->_('common.updated')),
 			'type' => Model_CustomField::TYPE_DATE,
 			'value' => $asset->updated_at,
 		);
